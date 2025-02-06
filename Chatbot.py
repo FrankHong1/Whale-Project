7aaf38c8586de2af5a4dcba87d17fe43b7121101
frankhong12b <39412132-frankhong12b@users.noreply.replit.com>
2025-02-06 09:56:20 +0000

    User checkpoint: Update chatbot to use gpt-4o-realtime-preview model and improve code formatting.

--- Chatbot.py
+++ Chatbot.py
@@ -1,17 +1,21 @@
 from openai import OpenAI
 import os
 
-client = OpenAI(api_key = os.getenv("OPENAI_API_KEY"))
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
 
 def reply(response):
   reply = response.choices[0].message.content
   return reply
 
+
 def chatting():
   msg_history = []
   system_msg = input("What type of chatbot would you like to create?\nUser: ")
   msg_history.append({"role": "system", "content": system_msg})
-  print("Bot: Your new assistant is ready, feel free to ask me anything!\nType 'exit' to end the chat\n")
+  print(
+      "Bot: Your new assistant is ready, feel free to ask me anything!\nType 'exit' to end the chat\n"
+  )
 
   while True:
     user_msg = input("\nUser: ")
@@ -19,15 +23,16 @@ def chatting():
       break
     msg_history.append({"role": "user", "content": user_msg})
     response = client.chat.completions.create(
-      model = "gpt-4o",
-      messages = msg_history
-    )
+        model="gpt-4o-realtime-preview",
+        #model = "model-ID"
+        messages=msg_history)
     ext_reply = reply(response)
     msg_history.append({"role": "assistant", "content": ext_reply})
     print(f"\nBot: {ext_reply}")
 
   print("\nBot: Goodbye!")
   return None
+
 
 if __name__ == "__main__":
   chatting()
