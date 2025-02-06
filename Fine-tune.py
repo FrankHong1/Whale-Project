ead60fed2fdcd669b48b71efced64a322b69c36f
Frank H <86214744+FrankHong1@users.noreply.github.com>
2025-02-06 14:25:30 +0000

    Initial push

--- Fine-tune.py
+++ Fine-tune.py
@@ -2,7 +2,7 @@ from openai import OpenAI
 import os
 import time
 
-#client = OpenAI(api_key = os.getenv("OPENAI_API_KEY"))
+client = OpenAI(api_key = os.getenv("OPENAI_API_KEY"))
 
 
 def upload_data(file_path):
